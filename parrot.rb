e57d7e1558b8da77b146edf1b9f27e046a27e19a
Luana Leal <luana.leal.eq@gmail.com>
2020-07-30 05:32:21 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts "Squawk!" 
+end  
+
+def parrot(phrase)
+  puts phrase
+end 
+
+def parrot(phrase)
+  return "Squawk!"
+end 
