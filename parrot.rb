5f1c02e78a3af7928838402f073f524caa7101ae
Luana Leal <luana.leal.eq@gmail.com>
2020-07-30 05:58:48 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -17,11 +17,13 @@
  # return phrase
 #end
 
-def parrot
-  puts "Squawk!"
-  return "Squawk!"
-end 
+
 def parrot(phrase)
   puts phrase
   return phrase
+end 
+
+def parrot
+  puts "Squawk!"
+  return "Squawk!"
 end 
